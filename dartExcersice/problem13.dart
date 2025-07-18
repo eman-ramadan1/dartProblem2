6e251746113dca0458dda99f8e937da843884133
eman-ramadan1 <er1337@fayoum.edu.eg>
2025-07-18 16:10:32 +0300

    Delete dartExcersice directory

--- dartExcersice/problem13.dart
+++
@@ -1,9 +0,0 @@
-
-void main(){
-
-List<int> numbers = [1, 2, 3, 4];
- List<int> odd= numbers.where((i) => i % 2 != 0).toList(); // using where to filter odd numbers
-int result= odd.reduce((a, b) => a + b); // using reduce to sum the odd numbers
-print(result); // prints the sum of the odd numbers
-
-}
--- dartExcersice/problem9.dart
+++
@@ -1,7 +0,0 @@
-
-void main() {
-  List<int>filter = [1, 2, 3, 4, 5, 6];
-  List<int> evens = filter.where((i) => i % 2 == 0).toList();  // using where to filter even numbers
-  print(evens);
-}
-// Filtering a list to get only even numbers
